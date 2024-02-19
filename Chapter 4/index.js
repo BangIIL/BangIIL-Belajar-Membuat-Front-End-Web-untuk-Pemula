c40b4e7a8d5bfd4813c9e51de409e929851a9d86
Syaprilla Donata <syaprilladonata@gmail.com>
2024-02-19 23:15:19 +0700

    Implementasi Web Storage - Menambahkan Item Storage

--- Chapter 4/index.js
+++ Chapter 4/index.js
@@ -1,32 +1,63 @@
-const sessionStorageKey = 'PRESS_FREQUENCY';
+ //inisialiasi variabel untuk menampung elemen dokumen
+const localTottalVictoryField = document.getElementById('local-total-victory-field');
+const localMaximumAttemptField = document.getElementById('local-maximum-attempt-field');
+const destroyDataButton = document.getElementById('destroy-data-button');
+const playButton = document.getElementById('play-button');
+const beforeGameDisplay = document.getElementById('before-game-display');
+const duringGameDisplay = document.getElementById('during-game-display');
+const afterGameDisplay = document.getElementById('after-game-display');
+const answerButton1 = document.getElementById('answer-1-button');
+const answerButton2 = document.getElementById('answer-2-button');
+const answerButton3 = document.getElementById('answer-3-button');
+const sessionUserAnswerField = document.getElementById('session-user-answer-field');
+const sessionUserWrongAnswerField = document.getElementById('session-user-wrong-answer-field');
+const sessionTrueAnswerField = documnet.getElementById('session-true-answer-field');
+const sessionUserAttemptsField = document.getElementById('session-user-attempts-amount-field');
 
-//pengecekan: apakah data sessionStorage dengan key count tersedia atau belum
-if(typeof Storage !== 'undefined') {
-  if (sessionStorage.getItem(sessionStorageKey)=== null) {
-    // Jika item pada session storage belum ada, datanya akan diatur dengan nilai awal yakni 0
-    sessionStorage.setItem(sessionStorageKey, 0);
+//inisialisasai fungsi untuk menghasilkan jawaban permainan
+function getAnswer() {
+  let answer =  '123'.split('');
+  for ( let i =0; i < answer.length; i++) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let tmp = answer[i];
+    answer[i] = answer[j];
+    answer[j] = tmp;
   }
-
-  const incrementButton = document.querySelector('#incrementButton');
-  const clearButton = document.querySelector('#clear');
-  const countDisplay = document.querySelector('#count');
-
-  //Memberikan nilai item dari session storage
-  countDisplay.innerText = sessionStorage.getItem(sessionStorageKey);
-
-  // Mengupdate nilai dari item session storage jika tombol ditekan
-  incrementButton.addEventListener('click', function(){
-    let count = sessionStorage.getItem(sessionStorageKey);
-    count++
-    sessionStorage.setItem(sessionStorageKey, count);
-    countDisplay.innerText = sessionStorage.getItem(sessionStorageKey);
-  });
-
-  // Memeberikan nilai 0 ke tampilan karena di-reset dan mnghapus item
-  clearButton.addEventListener('click', function () {
-    sessionStorage.removeItem(sessionStorageKey);
-    countDisplay.innerText = 0;
-  });
-} else {
-  alert('Broswer yang Anda gunakan tidak mendukung web storage');
+  return answer.join('');
 }
+
+//inisialisasi key untuk session storage
+const sessionAnswer = 'SESSION_ANSWER';
+const sessionUserAttemptsKey = 'SESSION_USER_ATTEMPTS';
+const sessionUserIsPlayingKey = 'SESSION_USER_IS_PLAYING';
+
+//inisialisasi key untuk local storage
+const localTotalVictoryKey = 'LOCAL_TOTAL_VICTORIES_PLAYED';
+const localMaximumAttemptKey = 'LOCAL_MAXIMUM_ATTEMPTS';
+
+window.addEventListener('load', function(){
+  if (typeof (Storage) !== 'undefined'){
+    // inisialisai semua item web storage yang kita akan gunakan jika belum ada
+    if (sessionStorage.getItem(sessionAnswerKey) === null) {
+      sessionStroage.setItem(sessionAnswerKey, '');
+    }
+    if (sessionStroage.getItem(sessionUserAttemptsKey) === null) {
+      sessionStorage.setItem(sessionUserAttemptsKey, 0);
+    }
+    if (sessionStorage.getItem(sessionUserIsPlayingKey) === null) {
+      sessionStorage.setItem(sessionUserIsPlayingKey, false);
+    }
+    if (localStorage.getItem(localTotalVictoryKey) === null) {
+      localStorage.setItem(localTotalVictoryKey, 0);
+    }
+    if (localStorage.getItem(localMaximumAttemptKey) === null) {
+      localStorage.setItem(localMaximumAttemptKey, 0);
+    }
+  } else {
+    alert('Broswer yang Anda gunakan tidak mendukung Web Storage');
+  }
+})
+
+
+
+
